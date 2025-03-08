0924e22df5ff8c8352edbb96b25a60e4d1c26cb7
Parvinder Singh <91476509+parvinder8607@users.noreply.github.com>
2025-03-08 15:03:03 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './static/js/bundle.js',
+  './static/css/main.css',
+  './favicon.ico',
+  './logo192.png',
+  './manifest.json',
+];
+
+// Install Service Worker and Cache Files
+self.addEventListener('install', event => {
+    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets...');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch Cached Resources When Offline
+self.addEventListener('fetch', event => {
+    clients.claim();
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request).then(response => response || caches.match('/'))
+    )
+  );
+});
+
+// Cleanup Old Caches on Activation
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(cacheNames.filter(cache => cache !== CACHE_NAME).map(cache => caches.delete(cache)))
+    )
+  );
+});
